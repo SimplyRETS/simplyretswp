4dddb308879d1bf3c9ad2d85adffdcb25c4a2e3e
Cody Reichert <cody@reichertbrothers.com>
2014-12-27 22:52:35 -0600

    updates to the SimpleRetsApiHelper class - still developing
    - use localhost retsd server in the URLbuilder for now
    - add ApiRequest function to see what we get with the url we built
    and handle different responses
    - dummied residential markup generator function

--- simple-rets-api-helper.php
+++ simple-rets-api-helper.php
@@ -10,22 +10,48 @@
 
 /* Code starts here */
 
-class simpleRetsApiHelper {
+class SimpleRetsApiHelper {
 
     public static function retrieveRetsListings( $params ) {
-        $request_url = simpleRetsApiHelper::retsdRequestUrlBuilder( $params );
+        $request_url = SimpleRetsApiHelper::srRequestUrlBuilder( $params );
+        $request_response = SimpleRetsApiHelper::srApiRequest( $request_url );
+        $response_markup = SimpleRetsApiHelper::srResidentialGenerator( $request_response );
 
-        $content = 'get your listings here: ' . $request_url;
+
+        $br = '<br>';
+        $content = 'get your listings here: ' . $request_url . $br;
+        $content = $content . 'here are your listings: ' . $request_response . $br;
+        $content = $request_response;
 
         return $content;
     }
 
-    public static function retsdRequestUrlBuilder( $params ) {
-        $base_url = 'http://54.187.230.155/properties/';
-        $property_type = 'res/';
-        $request_url = $base_url . $property_type;
+    // takes parameters from the loaded post and creates the url we need to call
+    // the remote retsd api.
+    public static function srRequestUrlBuilder( $params ) {
+        // $base_url = 'http://54.187.230.155/properties/';
+        $base_url = 'http://localhost:3001/properties?authid=mojo&authkey=mojo&type=res';
 
-        return $request_url;
+        // run params through this filter to remove elements with empty values
+        $filters = array_filter( $params );
+
+        $filters_query = http_build_query( $filters );
+        $request_url = $base_url . $property_type . $start_query . $filters_query . '<br>';
+
+        return $base_url;
+    }
+
+    public static function srApiRequest( $url ) {
+        $request = wp_remote_retrieve_body( wp_remote_get( $url ) );
+        $response_json = json_decode( $request );
+
+        $response = $response_json;
+
+        return $response;
+    }
+
+    public static function srResidentialGenerator( $response ) {
+        echo 'we\'ll parse the property info here <br><br>';
     }
 
 }
