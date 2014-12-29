a3f905433f35bdacf80cd25a06bfe8d063c36bfc
Cody Reichert <cody@reichertbrothers.com>
2014-12-29 00:42:22 -0600

    fix up the Url Builder function in the Api Helper class
    added api credentials to request url

--- simple-rets-api-helper.php
+++ simple-rets-api-helper.php
@@ -13,32 +13,39 @@
 class SimpleRetsApiHelper {
 
     public static function retrieveRetsListings( $params ) {
-        $request_url = SimpleRetsApiHelper::srRequestUrlBuilder( $params );
+        $request_url      = SimpleRetsApiHelper::srRequestUrlBuilder( $params );
         $request_response = SimpleRetsApiHelper::srApiRequest( $request_url );
-        $response_markup = SimpleRetsApiHelper::srResidentialGenerator( $request_response );
+        $response_markup  = SimpleRetsApiHelper::srResidentialGenerator( $request_response );
 
 
         $br = '<br>';
-        $content = 'get your listings here: ' . $request_url . $br;
-        $content = $content . 'here are your listings: ' . $request_response . $br;
-        $content = $request_response;
+        $content = 'request_url: ' . $request_url . $br;
+        $content .= 'request_response: ' . $request_response . $br;
+        $content = $content . 'request_markup: ' . $response_markup . $br;
 
         return $content;
+
     }
 
     // takes parameters from the loaded post and creates the url we need to call
     // the remote retsd api.
     public static function srRequestUrlBuilder( $params ) {
-        // $base_url = 'http://54.187.230.155/properties/';
-        $base_url = 'http://localhost:3001/properties?authid=mojo&authkey=mojo&type=res';
+        $base_url = 'http://54.187.230.155/properties';
+        // $base_url = 'http://localhost:3001/properties';
+        $authid   = get_option( 'sr_api_name' );
+        $authkey  = get_option( 'sr_api_key' );
+        $prop_type = 'res';
+        // ^only residentials for now
+
+        // build enough of the url to authorize api
+        $auth_url = "{$base_url}?authid={$authid}&authkey={$authkey}&type={$prop_type}";
 
         // run params through this filter to remove elements with empty values
         $filters = array_filter( $params );
-
         $filters_query = http_build_query( $filters );
-        $request_url = $base_url . $property_type . $start_query . $filters_query . '<br>';
+        $request_url = "{$auth_url}&{$filters_query}";
 
-        return $base_url;
+        return $request_url;
     }
 
     public static function srApiRequest( $url ) {
