85ba9a5fbd1aec78a9ad15538ae4c08fcef2d333
Cody Reichert <cody@reichertbrothers.com>
2015-02-07 23:25:12 -0600

    added SimplyRETS Random Listing Widget
    This Widget takes a list of MLS Id's (comma separated) and displays
    a random one from the list on page load

--- simply-rets-widgets.php
+++ simply-rets-widgets.php
@@ -17,14 +17,15 @@
 
 /* Code starts here */
 function srRegisterWidgets() {
-    register_widget('sr_listing_widget');
+    register_widget('srFeaturedListingWidget');
+    register_widget('srRandomListingWidget');
 }
 
 
-class sr_listing_widget extends WP_Widget {
+class srFeaturedListingWidget extends WP_Widget {
 
 	/** constructor */
-	function sr_listing_widget() {
+	function srFeaturedListingWidget() {
 		parent::WP_Widget(false, $name = 'SimplyRETS Featured Listing');
 	}
 
@@ -71,6 +72,81 @@ class sr_listing_widget extends WP_Widget {
 		$title = apply_filters('widget_title', $instance['title']);
 		$mlsid = $instance['mlsid'];
 
+
+		$cont .= $before_widget;
+		// populate title
+		if( $title ) {
+			$cont .= $before_title . $title . $after_title;
+		} else {
+			$cont .= $before_title . "Featured Listing" .$after_title;
+		}
+
+		// populate content
+		if( $mlsid ) {
+			$cont .= SimplyRetsApiHelper::retrieveWidgetListing( $mlsid );
+		} else {
+			$cont = "No listing found";
+		}
+
+		$cont .= $after_widget;
+		echo $cont;
+	}
+
+}
+
+class srRandomListingWidget extends WP_Widget {
+
+	/** constructor */
+	function srRandomListingWidget() {
+		parent::WP_Widget(false, $name = 'SimplyRETS Random Listing');
+	}
+
+	/** save widget --  @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['mlsids'] = strip_tags($new_instance['mlsids']);
+		return $instance;
+	}
+
+	/** admin widget form --  @see WP_Widget::form */
+	function form( $instance ) {
+		$title  = esc_attr($instance['title']);
+		$mlsids = esc_attr($instance['mlsids']);
+
+		?>
+		<p>
+		  <label for="<?php echo $this->get_field_id('title'); ?>">
+			<?php _e('Title:'); ?>
+		  </label>
+		  <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+				 name="<?php echo $this->get_field_name('title'); ?>"
+				 type="text"
+				 value="<?php echo $title; ?>" />
+		</p>
+
+		<p>
+		  <label for="<?php echo $this->get_field_id('mlsids'); ?>">
+			<?php _e('MLS Id\'s (comma separated):'); ?>
+		  </label>
+		  <input class="widefat"
+				 id="<?php echo $this->get_field_id('mlsids'); ?>"
+				 name="<?php echo $this->get_field_name('mlsids'); ?>"
+				 type="text"
+				 value="<?php echo $mlsids; ?>" />
+		</p>
+		<?php
+	}
+
+	/** front end widget render -- @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		$title  = apply_filters('widget_title', $instance['title']);
+		$mlsids = $instance['mlsids'];
+        $mlsids_arr = explode( ',', $mlsids );
+
+        $mlsid = $mlsids_arr[array_rand($mlsids_arr)];
 
 		$cont .= $before_widget;
 		// populate title
