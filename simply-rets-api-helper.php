ea9af53cef789dffbad9e48c96f6bcc92ad52e79
Cody Reichert <cody@reichertbrothers.com>
2015-04-27 15:50:19 -0500

    remove Multi-Family from default property type

--- simply-rets-api-helper.php
+++ simply-rets-api-helper.php
@@ -117,7 +117,6 @@ class SimplyRetsApiHelper {
                 "Residential"
                 , "Condominium"
                 , "Rental"
-                , "Multi-Family"
             );
         }
 
