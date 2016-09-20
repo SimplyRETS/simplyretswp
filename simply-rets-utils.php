9ca807d24f2a9a44cfea5a7718c1b22357aa8635
Cody Reichert <cody@reichertbrothers.com>
2016-09-20 14:13:44 -0500

    Add polyfill for http_parse_headers function
    Used for parsing HTTP headers, the polyfill is compat for possible older
    versions of PHP

--- simply-rets-utils.php
+++ simply-rets-utils.php
@@ -270,3 +270,34 @@ HTML;
     }
 
 }
+
+
+/**
+ * Top level 'pollyfill' for 'http_parse_headers'
+ *
+ * Taken from PHP implementation:
+ * http://php.net/manual/it/function.http-parse-headers.php
+ */
+if (!function_exists('http_parse_headers')) {
+    function http_parse_headers ($raw_headers) {
+        $headers = array(); // $headers = [];
+
+        foreach (explode("\n", $raw_headers) as $i => $h) {
+            $h = explode(':', $h, 2);
+
+            if (isset($h[1])) {
+                if(!isset($headers[$h[0]])) {
+                    $headers[$h[0]] = trim($h[1]);
+                } else if(is_array($headers[$h[0]])) {
+                    $tmp = array_merge($headers[$h[0]],array(trim($h[1])));
+                    $headers[$h[0]] = $tmp;
+                } else {
+                    $tmp = array_merge(array($headers[$h[0]]),array(trim($h[1])));
+                    $headers[$h[0]] = $tmp;
+                }
+            }
+        }
+
+        return $headers;
+    }
+}
