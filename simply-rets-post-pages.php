dbc9e74293a56dd667254cb0e6c1d5f6ce5d2ae1
Cody Reichert <cody@reichertbrothers.com>
2018-03-26 17:01:11 -0500

    Fix uncaught exception in 'q' parameter search
    **Issue**
    Error: Uncaught Error: Unsupported operand types
    
    Basically, the code was - in some cases, trying to add a string and an
    array, which is invalid in PHP.
    
    The issue in the code was that `sr_q` was defaulted to an array -
    instead of the resolved value being _casted_ to an array. So, if a value
    was provided for `sr_q` it was never casted to an array and the
    exception was thrown.
    
    **Solution**
    More specifically cast `sr_q` and `sr_keywords` to arrays only after
    `get_query_var` has been called - which defaults to a string.
    
    - [x] Fix uncaught exception
    - [x] Run some test cases

--- simply-rets-post-pages.php
+++ simply-rets-post-pages.php
@@ -757,10 +757,9 @@ class SimplyRetsCustomPostPages {
              */
 
             // Combine sr_q and _keywords into 1 string.
-            $kw_string = implode(
-                "; ",
-                get_query_var('sr_q', array()) + array(get_query_var('sr_keywords', ''))
-            );
+            $sr_q = get_query_var('sr_q', '');
+            $sr_kw = get_query_var('sr_keywords', '');
+            $kw_string = implode("; ", array($sr_q) + array($sr_kw));
 
             $next_atts = $listing_params + array(
                 "q" => $kw_string,
