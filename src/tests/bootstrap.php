d30baaf8f9ae910eb5d6e1eba52678c4d93fe39c
Cody Reichert <cody@reichertbrothers.com>
2019-11-12 10:34:36 -0600

    Update tests

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -6,9 +6,9 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../simply-rets.php';
+    require dirname( __FILE__ ) . '/../simply-rets.php';
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-
