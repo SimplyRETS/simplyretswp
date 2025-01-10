e5d58b92d4483a1e2a6024fb5a5bd9fb408b8a76
Cody Reichert <cody@reichertbrothers.com>
2025-01-09 19:19:35 -0600

    Bump version number - v3.0.0
    Mainly includes major formatting any syntax upgrades per WordPress's
    plugin review requirements.

--- src/simply-rets.php
+++ src/simply-rets.php
@@ -4,7 +4,7 @@ Plugin Name: SimplyRETS Real Estate IDX
 Plugin URI: https://simplyrets.com/wordpress-idx-plugin
 Description: Show your Real Estate listings on your Wordpress site. SimplyRETS provides a very simple set up and full control over your listings.
 Author: SimplyRETS
-Version: 2.11.3
+Version: 3.0.0
 License: GNU General Public License v3 or later
 
 Copyright (c) SimplyRETS 2014 - 2024
@@ -12,7 +12,7 @@ Copyright (c) SimplyRETS 2014 - 2024
 */
 
 /* Code starts here */
-const SIMPLYRETSWP_VERSION = "v2.11.3";
+const SIMPLYRETSWP_VERSION = "v3.0.0";
 
 $plugin = plugin_basename(__FILE__);
 $php_version = phpversion();
