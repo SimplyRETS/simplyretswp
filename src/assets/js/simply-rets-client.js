283363068883f5ffbfa9f53442c343364117ce58
Cody Reichert <cody@reichertbrothers.com>
2025-01-29 18:01:37 -0600

    Fix [sr_map_search] not showing listing markers

--- src/assets/js/simply-rets-client.js
+++ src/assets/js/simply-rets-client.js
@@ -217,7 +217,7 @@ var makeMapMarkers = function(
         var lat = listing.geo.lat,
             lng = listing.geo.lng;
 
-        if(lat && lng && listing.internetAddressDisplay) {
+        if(lat && lng && listing.internetAddressDisplay !== false) {
 
             var bound  = new google.maps.LatLng(listing.geo.lat, listing.geo.lng);
 
